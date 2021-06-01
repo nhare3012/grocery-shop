ecaaa56b11f87b210a90bee76568c3feb03b3ce0
Tawanda Nhare <tawanda@uncommon.org>
2021-06-01 10:51:23 +0200

    Initial commit

---
+++ src/List.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const List = ({ items, removeItem, editItem }) => {
+  return (
+    <div className='grocery-list'>
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article className='grocery-item' key={id}>
+            <p className="title"> {title} </p>
+            <div className="btn-container">
+              <button type='button' className='edit-btn' onClick={ () => editItem (id) }   >edit</button>
+              <button type='button' className='delete-btn' onClick={ () => removeItem(id) } >delete</button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
